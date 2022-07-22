400d27124f9b284f340925757b92fb3d9e1e47e3
DaeHyeonHong <fholng@outlook.com>
2022-07-22 19:35:55 +0900

    Job

--- src/main/java/io/spring/springbatch/JobConfiguration.java
+++ src/main/java/io/spring/springbatch/JobConfiguration.java
@@ -13,33 +13,28 @@ import static org.springframework.batch.repeat.RepeatStatus.FINISHED;
 @Slf4j
 @Configuration
 @RequiredArgsConstructor
-public class DBJobConfiguration {
+public class JobConfiguration {
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
 
     @Bean
     public Job job() {
-        return this.jobBuilderFactory.get("job")
-                .start(this.step1())
-                .next(this.step2())
-                .build();
+        return this.jobBuilderFactory.get("job").start(this.step1()).next(this.step2()).build();
     }
 
-    private Step step1() {
-        return this.stepBuilderFactory.get("step1")
-                .tasklet((contribution, chunkContext) -> {
-                    log.info("Step1 was executed");
-                    return FINISHED;
-                })
-                .build();
+    @Bean
+    public Step step1() {
+        return this.stepBuilderFactory.get("step1").tasklet((contribution, chunkContext) -> {
+            log.info("Step1 was Executed");
+            return FINISHED;
+        }).build();
     }
 
-    private Step step2() {
-        return this.stepBuilderFactory.get("step2")
-                .tasklet((contribution, chunkContext) -> {
-                    log.info("Step2 was executed");
-                    return FINISHED;
-                })
-                .build();
+    @Bean
+    public Step step2() {
+        return this.stepBuilderFactory.get("step2").tasklet((contribution, chunkContext) -> {
+            log.info("Step2 was Executed");
+            return FINISHED;
+        }).build();
     }
 }
