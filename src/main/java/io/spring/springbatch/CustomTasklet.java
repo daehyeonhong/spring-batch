3faae33e9033a807f2d9bbc23c8f43b38f35a5e4
DaeHyeonHong <fholng@outlook.com>
2022-07-27 22:12:01 +0900

    Step

---
+++ src/main/java/io/spring/springbatch/CustomTasklet.java
@@ -0,0 +1,17 @@
+package io.spring.springbatch;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import static org.springframework.batch.repeat.RepeatStatus.FINISHED;
+
+@Slf4j
+public class CustomTasklet implements Tasklet {
+    @Override
+    public RepeatStatus execute(final StepContribution contribution, final ChunkContext chunkContext) {
+        log.info("Step2 was Executed");
+        return FINISHED;
+    }
+}
--- src/main/java/io/spring/springbatch/StepConfiguration.java
+++ src/main/java/io/spring/springbatch/StepConfiguration.java
@@ -13,13 +13,16 @@ import static org.springframework.batch.repeat.RepeatStatus.FINISHED;
 @Slf4j
 @Configuration
 @RequiredArgsConstructor
-public class JobExecutionConfiguration {
+public class StepConfiguration {
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
 
     @Bean
     public Job job() {
-        return this.jobBuilderFactory.get("Job").start(this.step1()).next(this.step2()).build();
+        return this.jobBuilderFactory.get("Job")
+                .start(this.step1())
+                .next(this.step2())
+                .build();
     }
 
     @Bean
@@ -32,10 +35,8 @@ public class JobExecutionConfiguration {
 
     @Bean
     public Step step2() {
-        return this.stepBuilderFactory.get("step2").tasklet((contribution, chunkContext) -> {
-            log.info("Step2 has Executed");
-//            throw new RuntimeException("Step2 has Failed");
-            return FINISHED;
-        }).build();
+        return this.stepBuilderFactory.get("step2")
+                .tasklet(new CustomTasklet())
+                .build();
     }
 }
