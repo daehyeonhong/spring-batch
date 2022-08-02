576c882f93363abe95301197052a02b55195fd86
DaeHyeonHong <fholng@outlook.com>
2022-08-03 00:12:12 +0900

    JobLauncher_Async/Sync Tests

--- src/main/java/io/spring/springbatch/JobLauncherController.java
+++ src/main/java/io/spring/springbatch/JobLauncherController.java
@@ -6,6 +6,7 @@ import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.JobParametersInvalidException;
+import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.batch.core.launch.support.SimpleJobLauncher;
 import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
 import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
@@ -22,6 +23,7 @@ import java.util.Date;
 @RequiredArgsConstructor
 public class JobLauncherController {
     private final Job job;
+    private final JobLauncher jobLauncher;
     private final BasicBatchConfigurer basicBatchConfigurer;
 
     @PostMapping(value = "/batch")
