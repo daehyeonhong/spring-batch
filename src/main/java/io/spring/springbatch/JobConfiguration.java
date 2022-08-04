f450ce2da71bd428ebeeb27eceeda73a1c99bb83
DaeHyeonHong <fholng@outlook.com>
2022-08-04 17:04:28 +0900

    JobConfiguration_1

--- src/main/java/io/spring/springbatch/JobConfiguration.java
+++ src/main/java/io/spring/springbatch/JobConfiguration.java
@@ -6,21 +6,22 @@ import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import static java.lang.Thread.sleep;
 import static org.springframework.batch.repeat.RepeatStatus.FINISHED;
 
 @Slf4j
 @Configuration
 @RequiredArgsConstructor
-public class JobLauncherConfiguration {
+public class JobConfiguration {
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
 
     @Bean
-    public Job job() {
-        return this.jobBuilderFactory.get("Job")
+    public Job batchJob1() {
+        return this.jobBuilderFactory.get("batchJob1")
+                .incrementer(new RunIdIncrementer())
                 .start(this.step1())
                 .next(this.step2())
                 .build();
@@ -30,7 +31,7 @@ public class JobLauncherConfiguration {
     public Step step1() {
         return this.stepBuilderFactory.get("step1")
                 .tasklet((contribution, chunkContext) -> {
-                    sleep(3000);
+                    log.info("Step1 has executed");
                     return FINISHED;
                 })
                 .build();
@@ -39,7 +40,10 @@ public class JobLauncherConfiguration {
     @Bean
     public Step step2() {
         return this.stepBuilderFactory.get("step2")
-                .tasklet((contribution, chunkContext) -> null)
+                .tasklet((contribution, chunkContext) -> {
+                    log.info("Step2 has executed");
+                    return FINISHED;
+                })
                 .build();
     }
 }
