4f601765dc089f961f33567604fba3990e44bbbd
DaeHyeonHong <fholng@outlook.com>
2024-02-19 20:06:15 +0900

    1. JobListener 사용

--- src/main/kotlin/com/spring/batch/JobRepositoryConfiguration.kt
+++ src/main/kotlin/com/spring/batch/JobRepositoryConfiguration.kt
@@ -2,31 +2,36 @@ package com.spring.batch
 
 import io.klogging.NoCoLogging
 import org.springframework.batch.core.Job
+import org.springframework.batch.core.JobExecutionListener
 import org.springframework.batch.core.Step
 import org.springframework.batch.core.job.builder.JobBuilder
 import org.springframework.batch.core.repository.JobRepository
 import org.springframework.batch.core.step.builder.StepBuilder
+import org.springframework.batch.core.step.tasklet.Tasklet
+import org.springframework.batch.repeat.RepeatStatus
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.transaction.PlatformTransactionManager
 
 
 @Configuration
-class ExecutionContextConfiguration(
+class JobRepositoryConfiguration(
     private val jobRepository: JobRepository,
     private val platformTransactionManager: PlatformTransactionManager,
-    private val executionContextTasklet1: ExecutionContextTasklet1,
-    private val executionContextTasklet2: ExecutionContextTasklet2,
-    private val executionContextTasklet3: ExecutionContextTasklet3,
-    private val executionContextTasklet4: ExecutionContextTasklet4,
+    private val jobExecutionListener: JobExecutionListener,
 ) : NoCoLogging {
+    private val executionContextTasklet1: Tasklet =
+        Tasklet { _, _ ->
+            logger.info { "Hello, World!" }
+            RepeatStatus.FINISHED
+        }
+
     @Bean
     fun job(): Job {
         return JobBuilder("job", this.jobRepository)
             .start(step1())
             .next(step2())
-            .next(step3())
-            .next(step4())
+            .listener(this.jobExecutionListener)
             .build()
     }
 
@@ -40,21 +45,7 @@ class ExecutionContextConfiguration(
     @Bean
     fun step2(): Step {
         return StepBuilder("step2", this.jobRepository)
-            .tasklet(this.executionContextTasklet2, this.platformTransactionManager)
-            .build()
-    }
-
-    @Bean
-    fun step3(): Step {
-        return StepBuilder("step3", this.jobRepository)
-            .tasklet(this.executionContextTasklet3, this.platformTransactionManager)
-            .build()
-    }
-
-    @Bean
-    fun step4(): Step {
-        return StepBuilder("step4", this.jobRepository)
-            .tasklet(this.executionContextTasklet4, this.platformTransactionManager)
+            .tasklet(this.executionContextTasklet1, this.platformTransactionManager)
             .build()
     }
 
