4f683d83e4718cd0ea585dba0499bc96424c595c
DaeHyeonHong <fholng@outlook.com>
2024-05-10 22:40:42 +0900

    - batch.job.enabled를 통한 자동 수행 관리 - 미사용 클래스 삭제 - Scope를 활용하여 지연생성 - 비동기처리

--- src/main/kotlin/com/spring/batch/JobLauncherController.kt
+++ src/main/kotlin/com/spring/batch/JobLauncherController.kt
@@ -3,8 +3,10 @@ package com.spring.batch
 import io.klogging.NoCoLogging
 import org.springframework.batch.core.Job
 import org.springframework.batch.core.JobParametersBuilder
-import org.springframework.batch.core.launch.JobLauncher
+import org.springframework.batch.core.configuration.support.DefaultBatchConfiguration
+import org.springframework.batch.core.launch.support.TaskExecutorJobLauncher
 import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.core.task.SimpleAsyncTaskExecutor
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
@@ -12,8 +14,8 @@ import java.time.LocalDate
 
 @RestController
 class JobLauncherController(
-    @Qualifier(value = "job") private val job: Job,
-    private val jobLauncher: JobLauncher,
+    @Qualifier(value = "defaultJob") private val job: Job,
+    private val defaultBatchConfiguration: DefaultBatchConfiguration,
 ) : NoCoLogging {
     @PostMapping("/launch")
     fun launch(@RequestBody member: Member): String {
@@ -21,8 +23,9 @@ class JobLauncherController(
         val jobParameters = JobParametersBuilder().addLong("id", member.id)
             .addLocalDate("date", LocalDate.now())
             .toJobParameters()
-
-        this.jobLauncher.run(job, jobParameters)
+        val taskExecutorJobLauncher = this.defaultBatchConfiguration.jobLauncher() as TaskExecutorJobLauncher
+        taskExecutorJobLauncher.setTaskExecutor(SimpleAsyncTaskExecutor())
+        taskExecutorJobLauncher.run(job, jobParameters)
         return "batch completed"
     }
 }
--- src/main/kotlin/com/spring/batch/JobRepositoryConfiguration.kt
+++ src/main/kotlin/com/spring/batch/JobRepositoryConfiguration.kt
@@ -1,57 +1,63 @@
 package com.spring.batch
 
 import io.klogging.NoCoLogging
-import org.springframework.batch.core.DefaultJobKeyGenerator
 import org.springframework.batch.core.Job
-import org.springframework.batch.core.JobExecutionListener
 import org.springframework.batch.core.Step
+import org.springframework.batch.core.configuration.annotation.JobScope
+import org.springframework.batch.core.configuration.annotation.StepScope
 import org.springframework.batch.core.job.builder.JobBuilder
 import org.springframework.batch.core.repository.JobRepository
-import org.springframework.batch.core.repository.support.JobRepositoryFactoryBean
 import org.springframework.batch.core.step.builder.StepBuilder
 import org.springframework.batch.core.step.tasklet.Tasklet
-import org.springframework.batch.item.database.support.DefaultDataFieldMaxValueIncrementerFactory
 import org.springframework.batch.repeat.RepeatStatus
+import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.transaction.PlatformTransactionManager
-import org.springframework.transaction.annotation.Isolation
-import javax.sql.DataSource
 
 
 @Configuration
 class JobRepositoryConfiguration(
-    private val jobRepository: JobRepository,
-    private val platformTransactionManager: PlatformTransactionManager,
-    private val jobExecutionListener: JobExecutionListener,
-    private val dataSource: DataSource,
 ) : NoCoLogging {
-    private val executionContextTasklet1: Tasklet =
-        Tasklet { _, _ ->
-            logger.info { "Hello, World!" }
-            RepeatStatus.FINISHED
-        }
-
-    @Bean
-    fun job(): Job {
-        return JobBuilder("job", this.jobRepository)
-            .start(step1())
-            .next(step2())
-            .listener(this.jobExecutionListener)
+    @Bean(name = ["defaultJob"])
+    fun job(
+        jobRepository: JobRepository,
+        @Qualifier(value = "step1") step1: Step,
+        @Qualifier(value = "step2") step2: Step,
+    ): Job {
+        logger.info { "Creating job" }
+        return JobBuilder("job", jobRepository)
+            .start(step1)
+            .next(step2)
             .build()
     }
 
-    @Bean
-    fun step1(): Step {
-        return StepBuilder("step1", this.jobRepository)
-            .tasklet(this.executionContextTasklet1, this.platformTransactionManager)
+    @JobScope
+    @Bean(name = ["step1"])
+    fun step1(
+        jobRepository: JobRepository,
+        platformTransactionManager: PlatformTransactionManager,
+    ): Step {
+        logger.info { "Creating step1" }
+        return StepBuilder("step1", jobRepository)
+            .tasklet(printGreeting(), platformTransactionManager)
             .build()
     }
 
-    @Bean
-    fun step2(): Step {
-        return StepBuilder("step2", this.jobRepository)
-            .tasklet(this.executionContextTasklet1, this.platformTransactionManager)
+    @JobScope
+    @Bean(name = ["step2"])
+    fun step2(
+        jobRepository: JobRepository,
+        platformTransactionManager: PlatformTransactionManager,
+    ): Step {
+        logger.info { "Creating step2" }
+        return StepBuilder("step2", jobRepository)
+            .tasklet(printGreeting(), platformTransactionManager)
             .build()
+    }
+
+    private fun printGreeting() = Tasklet { _, _ ->
+        logger.info { "Hello, World!" }
+        RepeatStatus.FINISHED
     }
 }
