0ad40482c95ab023f5f7810405209a00acc91d49
DaeHyeonHong <fholng@outlook.com>
2024-06-13 21:52:51 +0900

    - JobParametersIncrementer -> 임의의 key를 활용하여 매 실행마다 다른 id 값을 전달하여 parameter 겹침을 피함으로 매번 job을 실행할수 있게 함

--- src/main/kotlin/com/spring/batch/IncrementerConfiguration.kt
+++ src/main/kotlin/com/spring/batch/IncrementerConfiguration.kt
@@ -1,8 +1,7 @@
 package com.spring.batch
 
 import io.klogging.NoCoLogging
-import org.springframework.batch.core.Job
-import org.springframework.batch.core.Step
+import org.springframework.batch.core.*
 import org.springframework.batch.core.job.builder.JobBuilder
 import org.springframework.batch.core.repository.JobRepository
 import org.springframework.batch.core.step.builder.StepBuilder
@@ -10,9 +9,10 @@ import org.springframework.batch.repeat.RepeatStatus
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.transaction.PlatformTransactionManager
+import java.time.LocalDateTime
 
 @Configuration
-class PreventRestartConfiguration(
+class IncrementerConfiguration(
     private val jobRepository: JobRepository,
     private val platformTransactionManager: PlatformTransactionManager,
 ) : NoCoLogging {
@@ -24,6 +24,7 @@ class PreventRestartConfiguration(
         .start(step1)
         .next(step2)
         .preventRestart()
+        .incrementer(CustomJobParametersIncrementer())
         .build()
 
     @Bean
@@ -37,6 +38,13 @@ class PreventRestartConfiguration(
     fun step2(
     ): Step = StepBuilder("step2", this.jobRepository).tasklet({ _, _ ->
         logger.info { "Hello, World!" }
-        throw IllegalArgumentException("Step2 was failed")
+        RepeatStatus.FINISHED
     }, this.platformTransactionManager).build()
+}
+
+class CustomJobParametersIncrementer : JobParametersIncrementer {
+    override fun getNext(parameters: JobParameters?): JobParameters {
+        val id = LocalDateTime.now()
+        return JobParametersBuilder().addLocalDateTime("run.id", id).toJobParameters()
+    }
 }
