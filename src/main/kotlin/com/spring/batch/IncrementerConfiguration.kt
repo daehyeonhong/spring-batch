740b080972d19c1b5a92f7b46291a75ebbe3bd70
DaeHyeonHong <fholng@outlook.com>
2024-06-13 21:55:36 +0900

    - RunIncrementer를 활용하여 자동 생성  - 다만 run.id에 할당된 type이 Long이므로, 다른 타입(가령 localDateTime)등을 사용했다면 `Invalid value for parameter run.id`오류 발생 가능

--- src/main/kotlin/com/spring/batch/IncrementerConfiguration.kt
+++ src/main/kotlin/com/spring/batch/IncrementerConfiguration.kt
@@ -2,16 +2,15 @@ package com.spring.batch
 
 import io.klogging.NoCoLogging
 import org.springframework.batch.core.Job
-import org.springframework.batch.core.JobParametersBuilder
 import org.springframework.batch.core.Step
 import org.springframework.batch.core.job.builder.JobBuilder
+import org.springframework.batch.core.launch.support.RunIdIncrementer
 import org.springframework.batch.core.repository.JobRepository
 import org.springframework.batch.core.step.builder.StepBuilder
 import org.springframework.batch.repeat.RepeatStatus
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.transaction.PlatformTransactionManager
-import java.time.LocalDateTime
 
 @Configuration
 class IncrementerConfiguration(
@@ -26,7 +25,7 @@ class IncrementerConfiguration(
         .start(step1)
         .next(step2)
         .preventRestart()
-        .incrementer { JobParametersBuilder().addLocalDateTime("run.id", LocalDateTime.now()).toJobParameters() }
+        .incrementer(RunIdIncrementer())
         .build()
 
     @Bean
